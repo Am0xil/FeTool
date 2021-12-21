2b4adf1e0d5e96f9210cc5e1ccb9ae33236a0631
amoxil <spark_wang@yeah.net>
2021-12-21 23:46:30 +0800

    【Music】修改网易云相关接口

--- Player/CloudMusic.py
+++ Player/CloudMusic.py
@@ -53,15 +53,9 @@ def c(text):
     e = '010001'
     f = '00e0b509f6259df8642dbc35662901477df22677ec152b5ff68ace615bb7b725152b3ab17a876aea8a5aa76d2e417629ec4ee341f56135fccf695280104e0312ecbda92557c93870114af6c9d05c4f7f0c3685b7a46bee255932575cce10b424d813cfe4875d3e82047b97ddef52741d546b8e289dc6935b3ece0462db0a22b8e7'
     text = text[::-1]
-    # print(text)
-    # print(int(binascii.hexlify(text.encode()), 16))
-    print(int(e, 16))
-    # print(int(f, 16))
     result = pow(int(binascii.hexlify(text.encode()), 16), int(e, 16), int(f, 16))
     return format(result, 'x').zfill(131)
 
-if __name__ == '__main__':
-    print(c('12345'))
 
 # 获取最终的参数 params 和 encSecKey 的方法
 def get_final_param(text, str):
